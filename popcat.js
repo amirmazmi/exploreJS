d8665f37099823f34b6a803fa6d4c1ce647b7dd4
amirmazmi <amirmazmi@users.noreply.github.com>
2021-09-14 12:03:00 +0800

    Add files via upload

---
+++ popcat.js
@@ -0,0 +1,33 @@
+var event = new KeyboardEvent('keydown', {
+	key: 'g',
+	ctrlKey: true
+});
+
+setInterval(function(){
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+	document.dispatchEvent(event);
+}, 0);
